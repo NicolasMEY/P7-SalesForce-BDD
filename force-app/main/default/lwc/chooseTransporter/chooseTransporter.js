0e3e3fdd778cf89200721f641cc689032e6f639a
nicolasmeyer <nsg.meyer@hotmail.fr>
2025-05-26 11:45:27 +0200

    🚧 chooseTransporter.js : Add logic to send and save transporter data, method saveTransporterChoice

--- force-app/main/default/lwc/chooseTransporter/chooseTransporter.js
+++ force-app/main/default/lwc/chooseTransporter/chooseTransporter.js
@@ -1,10 +1,12 @@
 import { LightningElement, api } from 'lwc';
-// importattion d'une variable booléen liée à la Custom Permission SF ==> Can_Send_Order
 import hasPermission from '@salesforce/customPermission/Can_Send_Order';
+import saveTransporterChoice from '@salesforce/apex/OrderTransporterController.saveTransporterChoice';
+// import getTransportOptions from '@salesforce/apex/TransporterSelector.getTransportOptions'; ..... à rajouter !!!!!!
+
 
 export default class ChooseTransporter extends LightningElement {
     @api recordId; // id de l'Order puisque l'on passe une commande
-    selectedOption = null; // Stock l'option radio sélectionnée
+    selectedOption = null; // Stocke l'option radio sélectionnée
     hasPermission = hasPermission; // Stocke la valeur importée pour simplifier l'accès dans le template
 
     options = [
@@ -12,17 +14,27 @@ export default class ChooseTransporter extends LightningElement {
         { label: 'Option la moins chère', value: 'cheapest' }
     ]; // Choix à afficher dans le radio group
 
-    handleChange(event) {
+    handleChange(event) { // à la sélection d'un option radio
         this.selectedOption = event.target.value;
     } // Stocke l'option radio dans selectedOption
 
-    handleSubmit() {
+    handleSubmit() { // au clic
         if (!this.hasPermission) {
             alert("Vous n'avez pas la permission d'envoyer une commande.");
             return;
         } // Méthode déclenchée au clic sur le bouton "Envoyer la commande"
-        // logique d’envoi ou de sauvegarde du transporteur
-        console.log('Commande envoyée avec :', this.selectedOption);
-        // !!!!!!!!! APEX ici pour enregistrer le choix !!!!!!!!!!!!!!
+        if (!this.selectedOption) {
+            alert("Veuillez sélectionner une option de livraison.");
+            return;
+        }
+        // Appel de la méthode Apex saveTransporterChoice ; logique d’envoi ou de sauvegarde du transporteur
+        saveTransporterChoice({ orderId: this.recordId, choice: this.selectedOption })
+        .then(() => {
+            alert("Commande envoyée avec succès !");
+        })
+        .catch(error => {
+            console.error(error);
+            alert("Erreur lors de l'envoi de la commande.");
+        });
     }
 }
